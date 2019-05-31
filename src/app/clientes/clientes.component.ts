c14ee74a3923ec3c1b04436315bc14907de61461
levymoreira <levy.moreira@intmed.com.br>
2019-05-30 22:18:05 -0300

    novos metodos nos services e adicao scss

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ClientService } from "../services/client.service";
+import { Cliente } from "../models/cliente.model";
 
 @Component({
-  selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.scss']
+  selector: "app-clientes",
+  templateUrl: "./clientes.component.html",
+  styleUrls: ["./clientes.component.scss"]
 })
 export class ClientesComponent implements OnInit {
-
-  constructor() { }
+  clientes: Array<Cliente> = [];
+  constructor(private clientService: ClientService) {}
 
   ngOnInit() {
+    this.listClientes();
   }
 
+  listClienteById() {}
+
+  listClientes() {
+    this.clientService.getClients().subscribe(res => {
+      console.log("chamando clientes", res);
+      this.clientes = res;
+    });
+  }
+
+  adicionarCliente(cliente) {
+    this.clientService.postCliente(cliente).subscribe(
+      res => {
+        console.log("res", res);
+      },
+      error => {
+        console.log("error", error);
+      }
+    );
+  }
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpEvent } from "@angular/common/http";
-import { tap } from "rxjs/operators";
+import { HttpClient, HttpEvent, HttpErrorResponse } from "@angular/common/http";
+import { tap, catchError } from "rxjs/operators";
+import { environment } from "../../environments/environment";
+import { Cliente } from "../models/cliente.model";
+import { throwError, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class ClientService {
-  baseUrl = "http://localhost:7000/";
+  baseUrl = `${environment.API}`;
 
   constructor(private httpClient: HttpClient) {}
 
@@ -20,5 +23,26 @@ export class ClientService {
         return data;
       })
     );
+  }
+
+  postCliente(cliente: Cliente): Observable<Cliente> {
+    return this.httpClient
+      .post<Cliente>(this.baseUrl + "api/clients/", cliente)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError("Something bad happened; please try again later.");
   }
 }
